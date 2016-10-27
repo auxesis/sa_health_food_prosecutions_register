0b4631078dd7881f52893b3e4d738f52ae36d375
Lindsay Holmwood <lindsay@holmwood.id.au>
2016-10-27 21:50:41 +1100

    Scraper gets all attrs bar offence nature

--- scraper.rb
+++ scraper.rb
@@ -8,6 +8,11 @@ require 'active_support/core_ext'
 # Set an API key if provided
 Geokit::Geocoders::GoogleGeocoder.api_key = ENV['MORPH_GOOGLE_API_KEY'] if ENV['MORPH_GOOGLE_API_KEY']
 
+def scrub(text)
+  text.gsub!(/[[:space:]]/, ' ') # convert all utf whitespace to simple space
+  text.strip
+end
+
 def get(url)
   @agent ||= Mechanize.new
   @agent.get(url)
@@ -16,9 +21,66 @@ end
 def extract_attrs(page)
   attrs = {}
 
+  # Address of business
   text = page.find {|e| e.text =~ /address of business/i}.text
   attrs['address'] = text[/^address of business:.(.*)/i, 1]
+  # FIXME(auxesis) needs multiline
   return nil if attrs['address'].blank?
+
+  # Trading name
+  text = page.find {|e| e.text =~ /trading name/i}.text
+  attrs['trading_name'] = text[/^trading name\*:.(.*)/i, 1]
+
+  # Name of convicted
+  text = page.find {|e| e.text =~ /name of convicted/i}.text
+  attrs['name_of_convicted'] = text[/^name of convicted\*:.(.*)/i, 1]
+  # FIXME(auxesis) needs multiline
+
+  # Date of offence
+  text = page.find {|e| e.text =~ /date of offence/i}.text
+  attrs['date_of_offence'] = text[/^date of offence:.(.*)/i, 1]
+
+  # Nature and circumstances of offence
+
+  # Court decision date
+  text = page.find {|e| e.text =~ /court decision date/i}.text
+  attrs['court_decision_date'] = text[/^court decision date:.(.*)/i, 1]
+
+  # Court
+  text = page.find {|e| e.text =~ /court:/i}.text
+  text = scrub(text).strip
+  attrs['court'] = text[/^court:.(.*)/i, 1]
+
+  # Prosecution brought by
+  text = page.find {|e| e.text =~ /prosecution brought by/i}.text
+  attrs['prosecution_brought_by'] = text[/^prosecution brought by:.(.*)/i, 1]
+
+  # Fine
+  text = page.find {|e| e.text =~ /fine:/i}.text
+  attrs['fine'] = text[/^(\d*\..)*fine:.(.*)/i, 2]
+
+  # Prosecution Costs
+  # Optional. Not all prosecutions have these.
+  if el = page.find {|e| e.text =~ /prosecution costs:/i}
+    text = el.text
+    attrs['prosecution_costs'] = text[/^prosecution costs:.(.*)/i, 1]
+  end
+
+  # Victim of Crime Levy
+  # 'victim' is singular and pluralised, so match on both because wtf
+  text = page.find {|e| e.text =~ /victims* of crime( levy)*:*/i}.text
+  attrs['victims_of_crime_levy'] = text[/^victims* of crime( levy)*:*.(.*)/i, 2]
+
+  # Total Penalty
+  text = page.find {|e| e.text =~ /total( penalty)*:/i}.text
+  attrs['total_penalty'] = text[/total([[:space:]]penalty)*:[[:space:]]*(.*)/i, 2]
+
+  # Comments
+  # Optional. Not all prosecutions have these.
+  if el = page.find {|e| e.text =~ /comments:/i}
+    text = el.text
+    attrs['comments'] = text[/^comments:.(.*)/i, 1]
+  end
 
   attrs
 end
@@ -94,8 +156,11 @@ def main
   puts "### There are #{new_prosecutions.size} new prosecutions"
 
   new_prosecutions.map! {|p| build_prosecution(p, page) }.compact!
-  new_prosecutions.map! {|n| geocode(n) }
+  new_prosecutions.map! {|p| geocode(p) }
 
+  binding.pry
+
+  exit
   # Serialise
   ScraperWiki.save_sqlite(['link'], new_prosecutions)
 
