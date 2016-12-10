d56a54071e9556b1d11d317d48dc6ea2bd04dcf5
Lindsay Holmwood <lindsay@holmwood.id.au>
2016-12-11 10:04:51 +1100

    Invoke scripts consistently

--- proxy/cibuild.sh
+++ proxy/cibuild.sh
@@ -3,9 +3,12 @@
 set -e
 set -x
 
+# Ensure we're in the same directory as this script
+cd $(dirname $0)
+
+# Set up Terraform
 wget https://releases.hashicorp.com/terraform/0.7.13/terraform_0.7.13_linux_amd64.zip
 unzip terraform_0.7.13_linux_amd64.zip
-
 mkdir $(pwd)/bin
 mv terraform bin
 export PATH="$PATH:$(pwd)/bin"
@@ -14,5 +17,3 @@ export PATH="$PATH:$(pwd)/bin"
 terraform remote config -backend=S3 -backend-config="bucket=$BUCKET" -backend-config="key=terraform.tfstate" -backend-config="region=us-east-1"
 
 make plan
-
-terraform remote push
